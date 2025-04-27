2c570138884baebe7bfd30b99904593a7897679c
CyberNet Labs Inc. <rupture_general_0a@icloud.com>
2025-04-27 18:52:50 +0200

    Add utils.py file

---
+++ mymodule/utils.py
@@ -0,0 +1,16 @@
+# utils.py
+"""
+Utility functions for the application.
+"""
+
+def greet_user(name):
+    """
+    Generates a greeting message for the user.
+    
+    Parameters:
+        name (str): Name of the user.
+    
+    Returns:
+        str: Greeting message.
+    """
+    return f"Hello, {name}! Welcome to the application."
